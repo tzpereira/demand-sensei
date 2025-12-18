107b93014e015bd2084db3b137f87d79a941392a
Mateus <mpdasilvatreze@gmail.com>
2025-12-17 21:33:38 -0300

    add conditional storage to the import route

--- backend/internal/http/routes/import.go
+++ backend/internal/http/routes/import.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 
 	"demand-sensei/backend/internal/http/handlers"
@@ -9,8 +11,29 @@ import (
 )
 
 func RegisterImportRoutes(r fiber.Router) {
-	storage := storage.NewLocalStorage()
-	service := services.NewImportService(storage)
+	var st storage.Storage
+	var err error
 
+	switch os.Getenv("STORAGE_DRIVER") {
+	case "s3":
+		st, err = storage.NewS3CompatibleStorage(
+			os.Getenv("S3_ENDPOINT"),
+			os.Getenv("S3_ACCESS_KEY"),
+			os.Getenv("S3_SECRET_KEY"),
+			os.Getenv("S3_BUCKET"),
+			"imports",
+			os.Getenv("S3_USE_SSL") == "true",
+		)
+	default:
+		st = storage.NewLocalStorage(
+			os.Getenv("LOCAL_UPLOAD_PATH"),
+		)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	service := services.NewImportService(st)
 	r.Post("/import", handlers.ImportHandler(service))
 }
