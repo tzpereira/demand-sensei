152ea4496ed0b75827904a439999d7a93b7680e3
Mateus <mpdasilvatreze@gmail.com>
2025-12-18 21:20:55 -0300

    remove filesystem storage, local works perfectly with minio

--- backend/internal/http/routes/import.go
+++ backend/internal/http/routes/import.go
@@ -14,22 +14,15 @@ func RegisterImportRoutes(r fiber.Router) {
 	var st storage.Storage
 	var err error
 
-	switch os.Getenv("STORAGE_DRIVER") {
-	case "s3":
-		st, err = storage.NewS3CompatibleStorage(
-			os.Getenv("S3_ENDPOINT"),
-			os.Getenv("S3_ACCESS_KEY"),
-			os.Getenv("S3_SECRET_KEY"),
-			os.Getenv("S3_BUCKET"),
-			"imports",
-			os.Getenv("S3_USE_SSL") == "true",
-		)
-	default:
-		st = storage.NewLocalStorage(
-			os.Getenv("LOCAL_UPLOAD_PATH"),
-		)
-	}
-
+	st, err = storage.NewS3CompatibleStorage(
+		os.Getenv("S3_ENDPOINT"),
+		os.Getenv("S3_ACCESS_KEY"),
+		os.Getenv("S3_SECRET_KEY"),
+		os.Getenv("S3_BUCKET"),
+		"imports",
+		os.Getenv("S3_USE_SSL") == "true",
+	)
+	
 	if err != nil {
 		panic(err)
 	}
