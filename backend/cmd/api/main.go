64aa540e90e167bc6c1dd7f1e4ad865a4254aba0
Mateus <mpdasilvatreze@gmail.com>
2025-12-23 23:37:04 -0300

    initialize a single publisher in the app main file
    + update env

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"demand-sensei/backend/internal/events/producer"
+	"demand-sensei/backend/internal/http/deps"
 	"demand-sensei/backend/internal/http/routes/router"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	app := fiber.New()
-	router.Register(app)
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
 
-	log.Println("API Gateway running on :9001")
-	log.Fatal(app.Listen(":9001"))
+	defer stop()
+
+	brokers := os.Getenv("KAFKA_BROKERS")
+
+	kafkaProducer, err := producer.NewProducer(
+		ctx,
+		[]string{brokers},
+	)
+
+	if err != nil {
+		log.Fatalf("failed to init kafka producer: %v", err)
+	}
+	defer kafkaProducer.Close()
+
+	app := fiber.New()
+
+	router.Register(app, deps.Deps{
+		Producer: kafkaProducer,
+	})
+
+	log.Println("API running on :9001")
+	if err := app.Listen(":9001"); err != nil {
+		log.Fatalf("server error: %v", err)
+	}
 }
