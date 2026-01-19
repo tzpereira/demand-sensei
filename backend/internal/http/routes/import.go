4ddf789ee5f12befe66c8be143ce63381e363ad8
Mateus <mpdasilvatreze@gmail.com>
2026-01-18 23:28:59 -0300

    add dedicated sales route

--- backend/internal/http/routes/import.go
+++ backend/internal/http/routes/import.go
@@ -1,15 +1,15 @@
 package routes
 
 import (
-	"github.com/gofiber/fiber/v2"
-
 	"demand-sensei/backend/internal/http/deps"
 	"demand-sensei/backend/internal/http/handlers"
 	"demand-sensei/backend/internal/services"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func RegisterImportRoutes(r fiber.Router, d deps.Deps) {
 	service := services.NewImportService(d.Storage, d.Producer)
 
-	r.Post("/import", handlers.ImportHandler(service))
+    r.Post("/importsales", handlers.ImportHandler(service, "sales"))
 }
